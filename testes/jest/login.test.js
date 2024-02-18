cb535cf8c8f8d68e53b95bdbe27da93f9841d37b
Cesar Temudo <cesarneves@student.dei.uc.pt>
2024-02-18 10:44:36 +0000

    add postman

--- testes/jest/login.test.js
+++ testes/jest/login.test.js
@@ -1,11 +1,16 @@
 const login = require("../../frontend/Pages/login");
 
 test("User logado com sucesso", async () => {
-    const response = await login.login("user1", "123B5");
+    const response = await login.login("user_teste", "123A5");
     expect(response).toBe(true);
 });
 
 test("Login falhado", async () => {
     const response = await login.login("user_invalido","password_inválida");
+    expect(response).toBe(false);
+});
+
+test("User com password errada", async () => {
+    const response = await login.login("user_teste", "178");
     expect(response).toBe(false);
 });
--- testes/jest/signup.test.js
+++ testes/jest/signup.test.js
@@ -6,30 +6,40 @@ test("User registado com sucesso", async () => {
 });
 
 test("Registo falhado", async () => {
+    const response = await signup.register("", "","user_1@abc.pt","user","1","974185261","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn6la0dOXP7G7oy6b1-xmmmZSTvY2y612sYg&usqp=CAU");
+    expect(response).toBe(400);
+});
+
+test("Password inválida", async () => {
     const response = await signup.register("user123teste", "123","user_1@abc.pt","user","1","974185261","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn6la0dOXP7G7oy6b1-xmmmZSTvY2y612sYg&usqp=CAU");
     expect(response).toBe(400);
 });
 
 test("Email já usado", async () => {
-    const response = await signup.register("user1teste", "123B5","user1@abc.pt","user","1","974185260","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn6la0dOXP7G7oy6b1-xmmmZSTvY2y612sYg&usqp=CAU");
+    const response = await signup.register("mnbbs321", "123B5","user_teste@abc.pt","user","1","974185260","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn6la0dOXP7G7oy6b1-xmmmZSTvY2y612sYg&usqp=CAU");
     expect(response).toBe(403);
 });
 
 test("Username já existe", async () => {
-    const response = await signup.register("user1", "123B5","user_1teste@abc.pt","user","1","974185268","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn6la0dOXP7G7oy6b1-xmmmZSTvY2y612sYg&usqp=CAU");
+    const response = await signup.register("user_teste", "123B5","user_1teste@abc.pt","user","1","974185268","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn6la0dOXP7G7oy6b1-xmmmZSTvY2y612sYg&usqp=CAU");
     expect(response).toBe(409);
+});
+
+test("Contacto já existe", async () => {
+    const response = await signup.register("poiouiiy789", "123B5","poiouiiy789e@abc.pt","user","1","912345678","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn6la0dOXP7G7oy6b1-xmmmZSTvY2y612sYg&usqp=CAU");
+    expect(response).toBe(400);
 });
 
 test("Obter todos os users", async () => {
     const users = await signup.getAllUsers();
     expect(users).toContainEqual({
-        email: "user1@abc.pt",
+        email: "user_teste@abc.pt",
         firstName: "user",
-        lastName: "1",
-        password: "123B5",
+        lastName: "teste",
+        password: "123A5",
         phoneNumber: "912345678",
-        photo: "https://tinyjpg.com/images/social/website.jpg",
+        photo: "https://a.storyblok.com/f/191576/1200x800/faa88c639f/round_profil_picture_before_.webp",
         userTasks: [],
-        username: "user1"
+        username: "user_teste"
     });
 });
